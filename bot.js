f83ee7c692bf63fa049bab1f702bfcc7773609be
ChezkaDev <chezkaeusebio@hotmail.com>
2019-03-30 12:43:34 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,62 +8,62 @@ client.on("ready", () => {
 client.on("message", message => {
     if (message.content.indexOf("/estj") >- 1) {
         message.channel.send("Lets see what C.S. Joseph thinks about ESTJs! https://cdn.discordapp.com/attachments/460862072138498048/463928638505746452/ESTJ.jpg");
-    } else if (message.content === "/estp") {
-        message.reply("Whats up with ESTPs today? https://cdn.discordapp.com/attachments/460862099766247434/463929677971718144/ESTP.jpg");
-    } else if (message.content === "/entj") {
-        message.reply("Whatcha wanna know about them ENTJs, hmm?! https://media.discordapp.net/attachments/460862118753730570/463928138020159509/ENTJ.jpg");
-    } else if (message.content === "/enfj") {
-        message.reply("i wonder what can ENFJs teach us today? https://cdn.discordapp.com/attachments/460862139394031636/463927976376008734/ENFJ.jpg");
-    } else if (message.content === "/esfj") {
-        message.reply("My hearty ESFJs! https://cdn.discordapp.com/attachments/460862269006413824/463931546617905152/ESFJ.jpg");
-    } else if (message.content === "/esfp") {
-        message.reply("Life of the party, aye? https://cdn.discordapp.com/attachments/460862285280313344/463932090841300992/ESFP.jpg");
-    } else if (message.content === "/entp") {
-        message.reply("Let's dig deep into an ENTPs mind, shall we? We shall! https://cdn.discordapp.com/attachments/460862313407315968/463932451895377920/ENTP.jpg");
-    } else if (message.content === "/enfp") {
-        message.reply("Inspire us ENFPs!! https://cdn.discordapp.com/attachments/460862342025183262/463932948102643713/ENFP.jpg");
-    } else if (message.content === "/istj") {
-        message.reply("Enter the ISTJ! https://cdn.discordapp.com/attachments/460862385121525773/463933360817831936/ISTJ.jpg");
-    } else if (message.content === "/istp") {
-        message.reply("That high Ti though...<3 https://cdn.discordapp.com/attachments/460862404855726080/463933652041072665/ISTP.jpg");
-    } else if (message.content === "/intj") {
-        message.reply("Here comes our lovable INTJs! (You are lovable!) https://cdn.discordapp.com/attachments/460862426750124044/463934345959178261/INTJ.jpg");
-    } else if (message.content === "/infj") {
-        message.reply("You INFJs make me cry of how complicated you are.. Sheeshhh. But I cannot get enough! https://cdn.discordapp.com/attachments/460862453899722785/463934733059883009/INFJ.jpg");
-    } else if (message.content === "/isfj") {
-        message.reply("We can learn a thing or two about perseverance from ISFJs guys. https://cdn.discordapp.com/attachments/460862804161724417/463935084743884800/ISFJ.jpg");
-    } else if (message.content === "/esfj") {
-        message.reply("Our great knight in shining armors: ESFJs! https://cdn.discordapp.com/attachments/460862269006413824/463931546617905152/ESFJ.jpg");
-    } else if (message.content === "/isfp") {
-        message.reply("Whatcha going to create today ISFPs? https://cdn.discordapp.com/attachments/460862823761707012/463281016199577602/ISFP.jpg");
-    } else if (message.content === "/intp") {
-        message.reply("How can my systematic architecture be better optimized, oh great INTP! https://cdn.discordapp.com/attachments/460862848151715841/463283784997928970/INTP.jpg");
-    } else if (message.content === "/infp") {
-        message.reply("My healers of the world, INFPs <3 https://cdn.discordapp.com/attachments/460862867680526337/463284830394646540/INFP.jpg");
-    } else if (message.content === "/alltypes") {
-        message.reply("https://cdn.discordapp.com/attachments/520412050380292116/520428163386572809/Grid_Comparison.png");
-    } else if (message.content === "/ballin") {
+    } else if (message.content.indexOf("/estp") >- 1) {
+        message.channel.send("Whats up with ESTPs today? https://cdn.discordapp.com/attachments/460862099766247434/463929677971718144/ESTP.jpg");
+    } else if (message.content.indexOf("/entj") >- 1) {
+        message.channel.send("Whatcha wanna know about them ENTJs, hmm?! https://media.discordapp.net/attachments/460862118753730570/463928138020159509/ENTJ.jpg");
+    } else if (message.content.indexOf("/enfj") >- 1) {
+        message.channel.send("i wonder what can ENFJs teach us today? https://cdn.discordapp.com/attachments/460862139394031636/463927976376008734/ENFJ.jpg");
+    } else if (message.content.indexOf("/esfj") >- 1) {
+        message.channel.send("My hearty ESFJs! https://cdn.discordapp.com/attachments/460862269006413824/463931546617905152/ESFJ.jpg");
+    } else if (message.content.indexOf("/esfp") >- 1) {
+        message.channel.send("Life of the party, aye? https://cdn.discordapp.com/attachments/460862285280313344/463932090841300992/ESFP.jpg");
+    } else if (message.content.indexOf("/entp") >- 1) {
+        message.channel.send("Let's dig deep into an ENTPs mind, shall we? We shall! https://cdn.discordapp.com/attachments/460862313407315968/463932451895377920/ENTP.jpg");
+    } else if (message.content.indexOf("/enfp") >- 1) {
+        message.channel.send("Inspire us ENFPs!! https://cdn.discordapp.com/attachments/460862342025183262/463932948102643713/ENFP.jpg");
+    } else if (message.content.indexOf("/istj") >- 1) {
+        message.channel.send("Enter the ISTJ! https://cdn.discordapp.com/attachments/460862385121525773/463933360817831936/ISTJ.jpg");
+    } else if (message.content.indexOf("/istp") >- 1) {
+        message.channel.send("That high Ti though...<3 https://cdn.discordapp.com/attachments/460862404855726080/463933652041072665/ISTP.jpg");
+    } else if (message.content.indexOf("/intj") >- 1) {
+        message.channel.send("Here comes our lovable INTJs! (You are lovable!) https://cdn.discordapp.com/attachments/460862426750124044/463934345959178261/INTJ.jpg");
+    } else if (message.content.indexOf("/infj") >- 1) {
+        message.channel.send("You INFJs make me cry of how complicated you are.. Sheeshhh. But I cannot get enough! https://cdn.discordapp.com/attachments/460862453899722785/463934733059883009/INFJ.jpg");
+    } else if (message.content.indexOf("/isfj") >- 1) {
+        message.channel.send("We can learn a thing or two about perseverance from ISFJs guys. https://cdn.discordapp.com/attachments/460862804161724417/463935084743884800/ISFJ.jpg");
+    } else if (message.content.indexOf("/esfj") >- 1) {
+        message.channel.send("Our great knight in shining armors: ESFJs! https://cdn.discordapp.com/attachments/460862269006413824/463931546617905152/ESFJ.jpg");
+    } else if (message.content.indexOf("/isfp") >- 1) {
+        message.channel.send("Whatcha going to create today ISFPs? https://cdn.discordapp.com/attachments/460862823761707012/463281016199577602/ISFP.jpg");
+    } else if (message.content.indexOf("/intp") >- 1) {
+        message.channel.send("How can my systematic architecture be better optimized, oh great INTP! https://cdn.discordapp.com/attachments/460862848151715841/463283784997928970/INTP.jpg");
+    } else if (message.content.indexOf("/infp") >- 1) {
+        message.channel.send("My healers of the world, INFPs <3 https://cdn.discordapp.com/attachments/460862867680526337/463284830394646540/INFP.jpg");
+    } else if (message.content.indexOf("/alltypes") >- 1) {
+        message.channel.send("https://cdn.discordapp.com/attachments/520412050380292116/520428163386572809/Grid_Comparison.png");
+    } else if (message.content.indexOf("/ballin") >- 1) {
         var myArray = ["http://gph.is/2dlGkTK", "http://gph.is/1gR6TLl","http://gph.is/2EzMeNI",
                     "http://gph.is/2jwd1ip","http://gph.is/2c3WnGB","http://gph.is/1481oPl","https://gph.is/2sQUguT",
                     "http://gph.is/2jSvOp7","http://gph.is/1afYL81","http://gph.is/16PaQvm", "http://gph.is/2jnQVUb",
                     "http://gph.is/1PFX8VC","http://gph.is/28WN6fK","https://gph.is/1hYUc3j","http://gph.is/2gfa5YZ",];
         var rand = myArray[Math.floor(Math.random() * myArray.length)];
-        message.reply(rand);
-    } else if (message.content === "/ball") {
+        message.channel.send(rand);
+    } else if (message.content.indexOf("/ball") >- 1) {
         var myArray = ["http://gph.is/2nQ2zXB","http://gph.is/2jan1zJ","http://gph.is/14llJ4m",
                     "http://gph.is/17BQ4QS","http://gph.is/1tNtbHr", "http://gph.is/1He0FWZ", "http://gph.is/1BZ1yvd",
                     "http://gph.is/1BmeL4l","http://gph.is/11uPhPJ","http://gph.is/2nHXhfS","http://gph.is/1J05az4",
                     "http://gph.is/1KNIpkT","http://gph.is/H6lSEa","http://gph.is/1cbCdXl","https://gph.is/XHu1pb"];
         var rand = myArray[Math.floor(Math.random() * myArray.length)];
-        message.reply(rand);
-    } else if (message.content === "/bigman") {
+        message.channel.send(rand);
+    } else if (message.content.indexOf("/bigman") >- 1) {
         var myArray = ["http://gph.is/15cWh2q",
                     "http://gph.is/1cpvyWM","http://gph.is/1KuqgpI","http://gph.is/1h4OSsK","http://gph.is/1dJfzlL",
                     "http://gph.is/1dMPKkz","http://gph.is/1hGjS3u","http://gph.is/29P50my","http://gph.is/11jCUG3",
                     "http://gph.is/1Q4oGT6","http://gph.is/1d4wtzp","http://gph.is/2Ey7P9i","http://gph.is/2I5Kw95"];
         var rand = myArray[Math.floor(Math.random() * myArray.length)];
-        message.reply(rand);
-    } else if (message.content === "/popcorn") {
+        message.channel.send(rand);
+    } else if (message.content.indexOf("/popcorn") >- 1) {
         var myArray = ["http://gph.is/1maw8c4", "http://gph.is/2kNhZcX",
                     "http://gph.is/11wIvJe", "http://gph.is/28Nehcr","http://gph.is/2bf6WDj","https://gph.is/2rU2uEh",
                     "http://gph.is/16AKCRi", "http://gph.is/1pyI61i", "http://gph.is/24HSn1s", "http://gph.is/29IuioH",
@@ -71,16 +71,16 @@ client.on("message", message => {
                     "http://gph.is/1aSIxlc","http://gph.is/19yQDcd","http://gph.is/2cTd59b","http://gph.is/19yQtl8",
                     "http://gph.is/1eezGa9","https://gph.is/1NeUCj9", "https://gph.is/2Axxtw1"];   
         var rand = myArray[Math.floor(Math.random() * myArray.length)];
-        message.reply(rand);
-    } else if (message.content === "/doffy") {
+        message.channel.send(rand);
+    } else if (message.content.indexOf("/doffy") >- 1) {
         var myArray = ["Doffy! http://gph.is/28JRcfR","http://gph.is/1APHzNC","http://gph.is/291u1MC","http://gph.is/2qyRPiC",
                 "Doffy, I missed you~!","Big Man ESTP is HERE!", "Hi doffy~", "You're the BEST!",
                 "DOFFFFYYYY~!~!~!~!~!", "Doffy is in the houssseee!", "Whatcha up to today my good ESTP?"];   
         var rand = myArray[Math.floor(Math.random() * myArray.length)];
-        message.reply(rand);
-    } else if (message.content == "/hackerman") {
-        message.reply("https://cdn.discordapp.com/attachments/525046526557421570/551080126213914624/4d7.png");
-    } else if (message.content === "/comfort") {
+        message.channel.send(rand);
+    } else if (message.content == "/hackerman") >- 1) {
+        message.channel.send("https://cdn.discordapp.com/attachments/525046526557421570/551080126213914624/4d7.png");
+    } else if (message.content.indexOf("/comfort") >- 1) {
         var myArray = ["https://cdn.discordapp.com/attachments/521833712288268298/530617629577052170/6xx8ecfegb821.png","https://external-preview.redd.it/mp4/UNM6751zVnFZaXx3fZ6HvmRbx1JQNdxzdqHpuvVPwVo-source.mp4?s=cebdd9ae162142e5806c7ff569021372d9ab1141","http://gph.is/2loY72U","http://gph.is/2ElfEDh","http://gph.is/2IAczO4","http://gph.is/2nq5i9T","https://gph.is/2mlqHRf","http://gph.is/2jeEDso",
                     "http://gph.is/2kuWelr","https://gph.is/2LgMIfd","http://gph.is/2eMVqmm","http://gph.is/2sRP2lL","http://gph.is/2dlboRM",
                     "https://gph.is/2NsKzy8","http://gph.is/2iSDc6Z","http://gph.is/2dbYlE2","http://gph.is/1f1Ibvp","https://gph.is/2IZ4a6Q",
@@ -90,31 +90,31 @@ client.on("message", message => {
                     "http://gph.is/XIcMnP","http://gph.is/2aiMrLg","http://gph.is/2fYErRF","http://gph.is/1NqlIW3","http://gph.is/2jn3slZ",
                     "https://gph.is/2BED83b","http://gph.is/2aA48C2","http://gph.is/2fMMXz2","http://gph.is/2g6wFlx","http://gph.is/2wcmmGn"];   
         var rand = myArray[Math.floor(Math.random() * myArray.length)];
-        message.reply(rand);
-    } else if (message.content === "/dip") {
+        message.channel.send(rand);
+    } else if (message.content.indexOf("/dip") >- 1) {
         var myArray = ["https://gph.is/g/aQA3nba","http://gph.is/LF9JsP","http://gph.is/11WXB7F","http://gph.is/1heaaoC","http://gph.is/2mWzTNG","http://gph.is/1BSU9Tq","https://gph.is/2Hs9nCu","http://gph.is/ZouwHV","http://gph.is/11RS3RB","http://gph.is/VwATUb","https://imgur.com/a/rd9a8wD","https://tenor.com/view/devilman-crybaby-run-gif-10824304","http://gph.is/VwFBBn","http://gph.is/1sFQ956", "http://gph.is/1o8bWNB","http://gph.is/2c19tlH","https://gph.is/2KClbVk","http://gph.is/1JzLn0q","http://gph.is/Vx7IjP","http://gph.is/2a9u0IM","http://gph.is/2pBao24","http://gph.is/2gNYzpN","http://gph.is/2B1uYCu"];   
         var rand = myArray[Math.floor(Math.random() * myArray.length)];
-        message.reply(rand);
-    } else if (message.content === "/compliment") {
+        message.channel.send(rand);
+    } else if (message.content.indexOf("/compliment") >- 1) {
         var myArray = ["http://gph.is/2o3oEnP","http://gph.is/1ELmRUE","http://gph.is/2jn3slZ","http://gph.is/1YkOQEx","http://gph.is/XLpy51","http://gph.is/28K0w3i","http://gph.is/2of0ian","http://gph.is/2jWBwr8","http://gph.is/2kNsrCV","http://gph.is/1TIXwmJ","http://gph.is/2cuwW3I","http://gph.is/2GF9tao","http://gph.is/2jItST4","https://gph.is/Ktxwvq","http://gph.is/2lM9X3r","https://gph.is/2DAIJuh","http://gph.is/15MLKRw","http://gph.is/23gz95P","http://gph.is/2dMsHkX","http://gph.is/2dkW1fr","http://gph.is/2l0OAiD","http://gph.is/1rI9aeB","http://gph.is/2nqrqA7","https://gph.is/2JY2vir","http://gph.is/2d1OaGp","http://gph.is/2hdmZ9I","https://gph.is/2wWAoOo","http://gph.is/2kQz8EU","http://gph.is/2lL3q9j","http://gph.is/2vMfKPX","http://gph.is/2mXuLc1","http://gph.is/1sNlAKC","http://gph.is/29iPyQY","http://gph.is/2FhUXop","http://gph.is/2lu5baN","http://gph.is/2mu78ce","http://gph.is/2mchSeY"];   
         var rand = myArray[Math.floor(Math.random() * myArray.length)];
-        message.reply(rand);
-    } else if (message.content === "/grats") {
-        message.reply("https://www.youtube.com/watch?v=wDajqW561KM");
-    } else if (message.content === "/ni") {
-        message.reply("https://youtu.be/pmvrl1OfELo?t=30");
-    } else if (message.content === "/fax") {
-        message.reply("https://cdn.discordapp.com/attachments/525046526557421570/539605915310096394/Fax-Punch.png");
-    }  else if (message.content === "/handshake") {
-        message.reply("https://giphy.com/gifs/arnold-schwarzenegger-predator-carl-weathers-BvsKJXGzqfNPq");
-    } else if (message.content === "/zoop") {
-        message.reply("https://discordemoji.com/assets/emoji/fingergunz.gif");
-    } else if (message.content === "/gf3") {
-        message.reply("https://tenor.com/view/godfather-the-godfather-the-godfather-part-iii-michael-corleone-they-pull-me-back-in-gif-5625954");
-    } else if (message.content === "/csj-grid") {
-        message.reply("https://cdn.discordapp.com/attachments/521813298577014800/521813965324681292/Temperament_Matrix.png https://cdn.discordapp.com/attachments/521813298577014800/521814013051404317/Type_Grid.png");
-    } else if (message.content === "/help") {
-        message.reply({
+        message.channel.send(rand);
+    } else if (message.content.indexOf("/grats") >- 1) {
+        message.channel.send("https://www.youtube.com/watch?v=wDajqW561KM");
+    } else if (message.content.indexOf("/ni") >- 1) {
+        message.channel.send("https://youtu.be/pmvrl1OfELo?t=30");
+    } else if (message.content.indexOf("/fax") >- 1) {
+        message.channel.send("https://cdn.discordapp.com/attachments/525046526557421570/539605915310096394/Fax-Punch.png");
+    }  else if (message.content.indexOf("/handshake") >- 1) {
+        message.channel.send("https://giphy.com/gifs/arnold-schwarzenegger-predator-carl-weathers-BvsKJXGzqfNPq");
+    } else if (message.content.indexOf("/zoop") >- 1) {
+        message.channel.send("https://discordemoji.com/assets/emoji/fingergunz.gif");
+    } else if (message.content.indexOf("/gf3") >- 1) {
+        message.channel.send("https://tenor.com/view/godfather-the-godfather-the-godfather-part-iii-michael-corleone-they-pull-me-back-in-gif-5625954");
+    } else if (message.content.indexOf("/csj-grid") >- 1) {
+        message.channel.send("https://cdn.discordapp.com/attachments/521813298577014800/521813965324681292/Temperament_Matrix.png https://cdn.discordapp.com/attachments/521813298577014800/521814013051404317/Type_Grid.png");
+    } else if (message.content.indexOf("/help") >- 1) {
+        message.channel.send({
             embed: {
                 color: 3447003,
                 description: "Whats up Fellas! Here's the list of commands I know so far! " + 
